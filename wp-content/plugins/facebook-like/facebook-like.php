80b740b0290dd9d66ead0b30bba3e7f67718c531
BeingDeveloper96 <beingdeveloper96@gmail.com>
2019-11-30 09:59:21 +0500

    First Commit

---
+++ wp-content/plugins/facebook-like/facebook-like.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Facebook Like Button
+ * Plugin URI: https://github.com/BeingDeveloper96/fb_like_plugin
+ * Author: Mansoor Khan
+ * Author URI: https://github.com/BeingDeveloper96
+ * Version: 1.0
+ * Description: Provide a way to configure a Facebook Like and Share Button for your website.
+ */
+
+if(! defined('ABSPATH')) {
+    exit('No Direct Access Allowed');
+}
+
